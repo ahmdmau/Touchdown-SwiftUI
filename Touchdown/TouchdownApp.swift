69d8389f58a7963349629a429b91d5a3e02443d5
Ahmad Maulana <ahmad.maulana@mekari.com>
2022-12-18 21:51:23 +0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Ahmad Maulana on 18/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
