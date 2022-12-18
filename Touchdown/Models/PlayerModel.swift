6cdf0f3eb8d2ce27a6881783517a98bf73e64cc2
Ahmad Maulana <ahmad.maulana@mekari.com>
2022-12-18 23:24:58 +0700

    Create extension to decode json

---
+++ Touchdown/Models/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Ahmad Maulana on 18/12/22.
+//
+
+import Foundation
+
+struct PlayerModel: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
